f6098359ee4d7073a65536c1f6a31d4f6ca6ac3c
Naem-Nizam <nizamuddin15bd@gmail.com>
2023-06-25 16:21:52 +0600

    hover focus ring offset

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,7 +7,7 @@ const Cart = () => {
       <div className="max-w-sm mx-auto p-8 bg-white rounded-xl shadow-md space-y-2 sm:flex sm:justify-center sm:space-x-4">
         <div>
           <img
-            className="h-24 rounded-full mx-auto ring-4 ring-green-400 sm:flex-shrink-0"
+            className="h-24 rounded-full mx-auto ring-4 ring-green-400 sm:flex-shrink-0 transform hover:scale-105 duration-500"
             src={logo}
             alt=""
           />
@@ -19,7 +19,7 @@ const Cart = () => {
               Youtube Channal
             </p>
           </div>
-          <button className="px-4 py-1 border border-purple-200 text-sm rounded-full text-purple-600 font-semibold">
+          <button className="px-4 py-1 border border-purple-200 text-sm rounded-full text-purple-600 font-semibold hover:text-white hover:bg-purple-600 hover:border-transparent focus-outine-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-1">
             Visit now
           </button>
         </div>
