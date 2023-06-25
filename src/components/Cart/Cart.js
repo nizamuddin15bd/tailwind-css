3c3002bf3631dc19fd63df2d74bb6a4971fbcaea
Naem-Nizam <nizamuddin15bd@gmail.com>
2023-06-25 16:54:44 +0600

    dark-mode on and work

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,8 +3,8 @@ import logo from "../../image/nizam.jpg";
 
 const Cart = () => {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <div className="max-w-sm mx-auto p-8 bg-white rounded-xl shadow-md space-y-2 sm:flex sm:justify-center sm:space-x-4">
+    <div className="h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-950">
+      <div className="max-w-sm mx-auto p-8 bg-white rounded-xl shadow-md space-y-2 sm:flex sm:justify-center sm:space-x-4 dark:bg-gray-900 dark:hover:bg-gray-700">
         <div>
           <img
             className="h-24 rounded-full mx-auto ring-4 ring-green-400 sm:flex-shrink-0 transform hover:scale-105 duration-500"
@@ -14,8 +14,10 @@ const Cart = () => {
         </div>
         <div className="text-center space-y-2 sm:space-x-0">
           <div className="space-y-0.5">
-            <p className="text-lg font-semibold text-black">Learn with sumit</p>
-            <p className="text-gray-500 font-medium space-y-0.5">
+            <p className="text-lg font-semibold text-black dark:text-white">
+              Learn with sumit
+            </p>
+            <p className="text-gray-500 font-medium space-y-0.5 dark:text-gray-400">
               Youtube Channal
             </p>
           </div>
